f8d41ec9b8dd11772305303c852451ddd9e45e6e
David S. Jackson <deepbsd@yahoo.com>
2018-12-18 11:19:28 -0500

    creating dataset for cells

--- src/App.js
+++ src/App.js
@@ -10,17 +10,21 @@ class App extends Component {
             huPlayer: 'X',
             player: 'X',
             gameOver: false,
-            board: Array(9).fill(''),
+            board: Array(9).fill('')
         }
 
     }
 
 
     clicked(ev){
+        this.state.board[ev.target.dataset.cell] = this.state.player;
+
         ev.target.innerText = this.state.player;
         this.setState({
-            player: this.state.player === this.state.huPlayer ? this.state.aiPlayer : this.state.huPlayer
+            player: this.state.player === this.state.huPlayer ? this.state.aiPlayer : this.state.huPlayer,
+            board: this.state.board
         })
+        console.log("cell: ",ev.target.dataset.cell)
     }
 
   render() {
@@ -30,15 +34,15 @@ class App extends Component {
       <h1>Unbeatable Tic Tac Toe AI</h1>
 
         <div className="board" onClick={(ev)=>this.clicked(ev)}>
-                <div className="cell empty-top empty-left" id="0" dataset-cell="0"></div>
-                <div className="cell empty-top" id="1"            dataset-cell="1"></div>
-                <div className="cell empty-top empty-right" id="2" dataset-cell="2"></div>
-                <div className="cell empty-left" id="3"           dataset-cell="3"></div>
-                <div className="cell " id="4"                     dataset-cell="4"></div>
-                <div className="cell empty-right" id="5"          dataset-cell="5"></div>
-                <div className="cell empty-bottom empty-left" id="6" dataset-cell="6"></div>
-                <div className="cell empty-bottom" id="7"         dataset-cell="7"></div>
-                <div className="cell empty-bottom empty-right" id="8" dataset-cell="8"></div>
+                <div className="cell empty-top empty-left" id="0" data-cell="0"></div>
+                <div className="cell empty-top"            id="1" data-cell="1"></div>
+                <div className="cell empty-top empty-right" id="2" data-cell="2"></div>
+                <div className="cell empty-left"           id="3" data-cell="3"></div>
+                <div className="cell " id="4"                     data-cell="4"></div>
+                <div className="cell empty-right" id="5"          data-cell="5"></div>
+                <div className="cell empty-bottom empty-left" id="6" data-cell="6"></div>
+                <div className="cell empty-bottom" id="7"         data-cell="7"></div>
+                <div className="cell empty-bottom empty-right" id="8" data-cell="8"></div>
         </div>
 
 
